5ed782c479e2645ba470a85563e199d7030976e3
robertmukhtarov-kfu <rrmukhtarov@stud.kpfu.ru>
2022-05-03 01:09:57 +0300

    Initial Commit

---
+++ NotTeams/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotTeams
+//
+//  Created by Robert Mukhtarov on 03.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotTeams/NotTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotTeamsApp.swift
+//  NotTeams
+//
+//  Created by Robert Mukhtarov on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
